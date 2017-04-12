1716e377b9d9be98417d1e482fd5162175c13aac
Anthony Poon <anthony.poon@asia-minerals.com>
2017-04-12 16:30:07 +0800

    Add a config to set default survey uid

--- view/template/surveyHeader.php
+++ view/template/surveyHeader.php
@@ -11,10 +11,10 @@
         <ul>
             <!--<li>Account Setting</li>-->
             <?php if ($this->user->isAdmin) { ?>
-            <li><a href='report?uid=<?php echo $param["uid"] ?>'>Admin Panel</a></li>
+            <li><a href='report?uid=<?php echo SURVEY_UID ?>'>Admin Panel</a></li>
             <li><a href='admin'>Settings</a></li>
             <?php } else { ?>
-            <li><a href='report?uid=<?php echo $param["uid"] ?>'>Report</a></li>
+            <li><a href='report?uid=<?php echo SURVEY_UID ?>'>Report</a></li>
             <?php } ?>
             <li><a href='survey'>Main Page</a></li>            
             <li><a href='login?action=logout'>Logout</a></li>
